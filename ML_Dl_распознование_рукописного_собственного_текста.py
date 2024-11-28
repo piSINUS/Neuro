2acbeab93f6d1b66465c9af04104f10ad437d0c8
piSINUS <104928495+piSINUS@users.noreply.github.com>
2024-11-28 20:16:54 +0300

    Add files via upload

--- ML_Dl_распознование_рукописного_собственного_текста.py
+++ ML_Dl_распознование_рукописного_собственного_текста.py
@@ -1,173 +1,147 @@
-# python notebook for Make Your Own Neural Network
-# code for a 3-layer neural network, and code for learning the MNIST dataset
-# this version trains using the MNIST dataset, then tests on our own images
-# (c) Tariq Rashid, 2016
-# license is GPLv2
-import numpy
-# scipy.special for the sigmoid function expit()
-import scipy.special
-# library for plotting arrays
-import matplotlib.pyplot
-# helper to load data from PNG image files
-import imageio.v3
-# glob helps select multiple files using patterns
+import numpy as np
+import scipy as scy
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.misc
 import glob
-# neural network class definition
+
+# определение класса нс
 class neuralNetwork:
     
-    
-    # initialise the neural network
-    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
-        # set number of nodes in each input, hidden, output layer
+    def __init__(self,inputnodes,hiddennodes,outputnodes,learningrate):
+        # задаем кол - во узлов в входном,скрытом и выходном слое
         self.inodes = inputnodes
         self.hnodes = hiddennodes
         self.onodes = outputnodes
-        
-        # link weight matrices, wih and who
-        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
-        # w11 w21
-        # w12 w22 etc 
-        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
-        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
-
-        # learning rate
+        # Матрицы весовых коэфф-ов связейБ wih и who.
+        # Весовые коэфф-ты связей между узлом i и узлом j
+        # следующего слоя обозначены как  w_i_j
+        self.wih = (np.random.normal(0.0,pow(self.inodes,-0.5),(self.hnodes,self.inodes)))
+        self.who = (np.random.normal(0.0,pow(self.hnodes,-0.5),(self.onodes, self.hnodes)))
+        # коэффициент обучения 
         self.lr = learningrate
-        
-        # activation function is the sigmoid function
-        self.activation_function = lambda x: scipy.special.expit(x)
-        
-        pass
+        # использование сигмоиды в качестве функции активации
+        self.activation_function = lambda x:scy.special.expit(x) 
 
-    
-    # train the neural network
-    def train(self, inputs_list, targets_list):
-        # convert inputs list to 2d array
-        inputs = numpy.array(inputs_list, ndmin=2).T
-        targets = numpy.array(targets_list, ndmin=2).T
-        
-        # calculate signals into hidden layer
-        hidden_inputs = numpy.dot(self.wih, inputs)
-        # calculate the signals emerging from hidden layer
+        pass
+    # тренеровка нс
+    def train(self,inputs_list,targets_list):
+        # преобразование списка входных значений
+        # в двухмерный массив
+        inputs = np.array(inputs_list,ndmin = 2).T
+        targets = np.array(targets_list,ndmin=2).T
+        #  рассчитать входящие сигналы для скрытого слоя
+        hidden_inputs = np.dot(self.wih,inputs)
+        #  рассчитать исходящие сигналы для скрытого слоя
         hidden_outputs = self.activation_function(hidden_inputs)
-        
-        # calculate signals into final output layer
-        final_inputs = numpy.dot(self.who, hidden_outputs)
-        # calculate the signals emerging from final output layer
+
+        #  рассчитать входящие сигналы для выходного слоя 
+        final_inputs = np.dot(self.who, hidden_outputs)
+        #  рассчитать исодящие сигналы для выходного слоя 
         final_outputs = self.activation_function(final_inputs)
-        
-        # output layer error is the (target - actual)
-        output_errors = targets - final_outputs
-        # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
-        hidden_errors = numpy.dot(self.who.T, output_errors) 
-        
-        # update the weights for the links between the hidden and output layers
-        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
-        
-        # update the weights for the links between the input and hidden layers
-        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
-        
-        pass
 
-    
-    # query the neural network
-    def query(self, inputs_list):
-        # convert inputs list to 2d array
-        inputs = numpy.array(inputs_list, ndmin=2).T
-        
-        # calculate signals into hidden layer
-        hidden_inputs = numpy.dot(self.wih, inputs)
-        # calculate the signals emerging from hidden layer
+        # ошибки входного слоя = (целевое значение - фактическое значение)
+        output_errors = targets - final_outputs
+        # ошибки скрытого слоя - это ошибки output_errors, распределенные пропорционально весовым коэф связей и рекомбинированные на скрытых узлах
+        hidden_errors = np.dot(self.who.T,output_errors)
+       
+
+        #  обновить весовые коэф для связей между скрыт и входными слоями
+        self.who += self.lr * np.dot((output_errors*final_outputs *(1.0 - final_outputs)),np.transpose(hidden_outputs))
+        #  обновить весовые коэф для связей между входными и скрытым слоям
+        self.wih += self.lr * np.dot((hidden_errors*hidden_outputs *(1.0 - hidden_outputs)),np.transpose(inputs))
+
+        pass
+    # Опрос  нс 
+    def query(self,inputs_list):
+        # преобразование списка входных значений
+        # в двухмерный массив
+        inputs = np.array(inputs_list,ndmin=2).T
+
+        #  рассчитать входящие сигналы для скрытого слоя
+        hidden_inputs = np.dot(self.wih,inputs)
+        #  рассчитать исходящие сигналы для скрытого слоя
         hidden_outputs = self.activation_function(hidden_inputs)
-        
-        # calculate signals into final output layer
-        final_inputs = numpy.dot(self.who, hidden_outputs)
-        # calculate the signals emerging from final output layer
+
+        #  рассчитать входящие сигналы для выходного слоя
+        final_inputs = np.dot(self.who,hidden_outputs)
+         #  рассчитать исодящие сигналы для выходного слоя 
         final_outputs = self.activation_function(final_inputs)
         
         return final_outputs
-# number of input, hidden and output nodes
+
+# кол-во входных выходных скрытых узлов
 input_nodes = 784
-hidden_nodes = 200
+hidden_nodes = 100
 output_nodes = 10
 
-# learning rate
-learning_rate = 0.1
+# коэф обучения
+learning_rate = 0.3
 
-# create instance of neural network
-n = neuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
-# load the mnist training data CSV file into a list
-training_data_file = open("mnist_dataset/mnist_train.csv", 'r')
+# Экземпляр нс 
+n = neuralNetwork( input_nodes,hidden_nodes,output_nodes,learning_rate)
+
+# Загрузить в список текстовый набор CSV файло
+training_data_file = open('minst_datas/mnist_train_100.csv','r')
 training_data_list = training_data_file.readlines()
 training_data_file.close()
-# train the neural network
 
-# epochs is the number of times the training data set is used for training
-epochs = 10
-
+epochs = 2
 for e in range(epochs):
-    # go through all records in the training data set
+
+    # перебрать все записи в трен наборе данных 
     for record in training_data_list:
-        # split the record by the ',' commas
+        # получить список значений, используя символы запятой в качестве разделителей
         all_values = record.split(',')
-        # scale and shift the inputs
-        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-        # create the target output values (all 0.01, except the desired label which is 0.99)
-        targets = numpy.zeros(output_nodes) + 0.01
-        # all_values[0] is the target label for this record
-        targets[int(all_values[0])] = 0.99
-        n.train(inputs, targets)
+
+        # масштабировть и сместить входные значения
+        inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+
+        # желаемого маркерного значения, равного 0,99
+        targets = np.zeros(output_nodes) + 0.01
+        
+        #  all_values - целевое маркерное значение для данной записи
+        targets[int(all_values[0])]  = 0.99
+
+        n.train(inputs,targets)
         pass
     pass
-# our own image test data set
-our_own_dataset = []
+img_array = scipy.misc.imread()
+test_data_file = open("mnist_test_10.csv",'r')
+test_data_list  = test_data_file.readlines()
+test_data_file.close()
+# тестирование нс
+# журнал оценки работы сети, первоначально пустой
+scorecard = []
 
-# load the png image data as test data set
-for image_file_name in glob.glob('my_own_images/2828_my_own_?.png'):
-    
-    # use the filename to set the correct label
-    label = int(image_file_name[-5:-4])
-    
-    # load image data from png files into an array
-    print ("loading ... ", image_file_name)
-    img_array = imageio.v3.imread(image_file_name, mode='F')
-    
-    # reshape from 28x28 to list of 784 values, invert values
-    img_data  = 255.0 - img_array.reshape(784)
-    
-    # then scale data to range from 0.01 to 1.0
-    img_data = (img_data / 255.0 * 0.99) + 0.01
-    print(numpy.min(img_data))
-    print(numpy.max(img_data))
-    
-    # append label and image data  to test data set
-    record = numpy.append(label,img_data)
-    our_own_dataset.append(record)
-    
+#перебрать все записи в тестовом наборе данных 
+for record in test_data_list:
+    # получить список значений, используя символы запятой в качестве разделителей
+    all_values = record.split(',')
+    # правильгный ответ - ервое значение
+    correct_label = int(all_values[0])
+    print(correct_label,'Истинный маркер')
+    # масштабировать и сместить входные значения
+    inputs=(np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    # опрос сети
+    outputs = n.query(inputs)
+    # индекс наибольшего значения является маркерным значением
+    label = np.argmax(outputs)
+    print(label,'ответ сети')
+    # присоединить оценку ответа сети к концу списка
+    if (label == correct_label):
+        # в случае правельного ответа сети присоединить к списку значение 1
+        scorecard.append(1)
+    else:
+        # в случае правельного ответа сети присоединить к списку значение 0
+        scorecard.append(0)
+        pass
     pass
 
-# test the neural network with our own images
+scorecard_array = np.asarray(scorecard)
 
-# record to test
-item = 0
+print(f"эффективность = {scorecard_array.sum() / scorecard_array.size}")
+# a = n.query((np.asfarray(all_values[1:]) / 255.0 *0.99) + 0.01)
 
-# plot image
-matplotlib.pyplot.imshow(our_own_dataset[item][1:].reshape(28,28), cmap='Greys', interpolation='None')
-
-# correct answer is first value
-correct_label = our_own_dataset[item][0]
-# data is remaining values
-inputs = our_own_dataset[item][1:]
-
-# query the network
-outputs = n.query(inputs)
-print (outputs)
-
-# the index of the highest value corresponds to the label
-label = numpy.argmax(outputs)
-print("network says ", label)
-# append correct or incorrect to list
-if (label == correct_label):
-    print ("match!")
-else:
-    print ("no match!")
-    pass
+if __name__ == "__main__":
+    print("This script is in the top-level code environment")
